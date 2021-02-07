13aa6956dab3551588b279e9f5b6c86a71c29040
G-Ray-cell <77442505+G-Ray-cell@users.noreply.github.com>
2021-02-07 08:08:21 +0100

    Add files via upload

---
+++ notes-node/notes-node/asyn.js
@@ -0,0 +1,17 @@
+function resolveAfter5Seconds() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, 5000);
+    });
+  }
+  
+  async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter5Seconds();
+    console.log(result);
+    // expected output: "resolved"
+  }
+  
+  asyncCall();
+  
